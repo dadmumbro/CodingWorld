3f1a889d5d31bd26ad82a857f268c07bd0923427
Gokulramanan Soundararajan <31602253+gokulramanaa@users.noreply.github.com>
2018-09-26 21:05:44 -0400

    Update one.py

--- one.py
+++ one.py
@@ -143,3 +143,43 @@ class Solution:
         three = str(int(one) + int(two))
         four = (list(reversed(three)))
         return list(map(int, four))
+
+
+# Given a string, find the length of the longest substring without repeating characters.
+
+# Example 1:
+
+# Input: "abcabcbb"
+# Output: 3 
+# Explanation: The answer is "abc", with the length of 3. 
+# Example 2:
+
+# Input: "bbbbb"
+# Output: 1
+# Explanation: The answer is "b", with the length of 1.
+# Example 3:
+
+# Input: "pwwkew"
+# Output: 3
+# Explanation: The answer is "wke", with the length of 3. 
+#              Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+class Solution:
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if len(s)<1:
+            return 0 
+        sub,maxl = [],1
+        for i in s:
+            if i not in sub:
+                sub.append(i)
+            else:
+                if len(sub) > maxl:
+                    maxl = len(sub)
+                idx = sub.index(i)
+                sub = sub[idx+1:]
+                sub.append(i)
+        return(max(len(sub), maxl))
